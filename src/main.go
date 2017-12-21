a1d1025332ca2f8258c28f1a9e1d661d02b519a4
Jaume Capdevila Pérez <contact@jaumecapdevila.net>
2017-12-21 21:48:54 +0100

    Auth twitter connections and connect to mongodb

--- src/main.go
+++ src/main.go
@@ -2,8 +2,25 @@ package main
 
 import (
 	"fmt"
+	"log"
+
+	mgo "gopkg.in/mgo.v2"
 )
+
+var db *mgo.Session
 
 func main() {
 	fmt.Println("Hello world")
+}
+
+func dialDB() error {
+	var err error
+	log.Println("dialing mongodb: localhost")
+	db, err = mgo.Dial("twitter_votes_database")
+	return err
+}
+
+func closeDB() {
+	db.Close()
+	log.Println("Closed database connection")
 }
--- src/twittervotes/twitter.go
+++ src/twittervotes/twitter.go
@@ -2,13 +2,68 @@ package twittervotes
 
 import (
 	"io"
+	"log"
 	"net"
+	"net/http"
+	"net/url"
+	"strconv"
+	"sync"
 	"time"
+
+	"github.com/garyburd/go-oauth/oauth"
+	"github.com/joeshaw/envdecode"
+)
+
+var (
+	authClient *oauth.Client
+	creds      *oauth.Credentials
+)
+
+var (
+	authSetupOnce sync.Once
+	httpClient    *http.Client
 )
 
 var conn net.Conn
 var reader io.ReadCloser
 
+func makeRequest(req *http.Request, params url.Values) (*http.Response, error) {
+	authSetupOnce.Do(func() {
+		setupTwitterAuth()
+		httpClient = &http.Client{
+			Transport: &http.Transport{
+				Dial: dial,
+			},
+		}
+	})
+	formEnc := params.Encode()
+	req.Header.Set("Content-Type", "application/x-www-form-urlencode")
+	req.Header.Set("Content-Length", strconv.Itoa(len(formEnc)))
+	req.Header.Set("Authorization", authClient.AuthorizationHeader(creds, "POST", req.URL, params))
+	return httpClient.Do(req)
+}
+
+func setupTwitterAuth() {
+	var ts struct {
+		ConsumerKey    string `env:"SP_TWITTER_KEY,required"`
+		ConsumerSecret string `env:"SP_TWITTER_SECRET,required"`
+		AccessToken    string `env:"SP_TWITTER_ACCESSTOKEN,required"`
+		AccessSecret   string `env:"SP_TWITTER_ACCESSSECRET,required"`
+	}
+	if err := envdecode.Decode(&ts); err != nil {
+		log.Fatal(err)
+	}
+	creds = &oauth.Credentials{
+		Token:  ts.AccessToken,
+		Secret: ts.AccessSecret,
+	}
+	authClient = &oauth.Client{
+		Credentials: oauth.Credentials{
+			Token:  ts.ConsumerKey,
+			Secret: ts.ConsumerSecret,
+		},
+	}
+}
 func dial(netw, addr string) (net.Conn, error) {
 	if conn != nil {
 		conn.Close()
