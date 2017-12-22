4a02541d157d9821686f02083863d2f2f9d2ca5d
Jaume Capdevila <contact@jaumecapdevila>
2017-12-22 09:38:34 +0100

    Connect to twitter API and move all the db stuff to a new file

--- src/main.go
+++ src/main.go
@@ -2,40 +2,8 @@ package main
 
 import (
 	"fmt"
-	"log"
-
-	mgo "gopkg.in/mgo.v2"
 )
-
-var db *mgo.Session
-
-type poll struct {
-	Options []string
-}
 
 func main() {
 	fmt.Println("Hello world")
-}
-
-func dialDB() error {
-	var err error
-	log.Println("dialing mongodb: localhost")
-	db, err = mgo.Dial("twitter_votes_database")
-	return err
-}
-
-func closeDB() {
-	db.Close()
-	log.Println("Closed database connection")
-}
-
-func loadOptions() ([]string, error) {
-	var options []string
-	iter := db.DB("ballots").C("polls").Find(nil).Iter()
-	var p poll
-	for iter.Next(&p) {
-		options = append(options, p.Options...)
-	}
-	iter.Close()
-	return options, iter.Err()
 }
---
+++ src/twittervotes/persistence.go
@@ -0,0 +1,36 @@
+package twittervotes
+
+import (
+	"log"
+
+	mgo "gopkg.in/mgo.v2"
+)
+
+var db *mgo.Session
+
+type poll struct {
+	Options []string
+}
+
+func dialDB() error {
+	var err error
+	log.Println("dialing mongodb: localhost")
+	db, err = mgo.Dial("twitter_votes_database")
+	return err
+}
+
+func closeDB() {
+	db.Close()
+	log.Println("Closed database connection")
+}
+
+func loadOptions() ([]string, error) {
+	var options []string
+	iter := db.DB("ballots").C("polls").Find(nil).Iter()
+	var p poll
+	for iter.Next(&p) {
+		options = append(options, p.Options...)
+	}
+	iter.Close()
+	return options, iter.Err()
+}
